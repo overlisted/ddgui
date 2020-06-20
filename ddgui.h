c3d9766b0c80ded024814a1544c179df36517e69
Ilya Maximov <mail@overlisted.net>
2020-06-20 21:07:18 +0200

    move the app class to a separate header

---
+++ ddgui.h
@@ -0,0 +1,43 @@
+#include <gtkmm/application.h>
+#include <gtkmm/button.h>
+#include <gtkmm/spinbutton.h>
+#include <gtkmm/comboboxtext.h>
+#include <gtkmm/applicationwindow.h>
+#include <gtkmm/filechooserdialog.h>
+#include <gtkmm/builder.h>
+
+#include "dd.h"
+
+struct DDGUI: Gtk::Application {
+  struct Window: Gtk::ApplicationWindow {
+    #define ID_SELECT_FILE "file-select"
+    #define STRING_SELECT_FILE "Select a file..."
+
+    Gtk::ComboBoxText* sourceSelect = nullptr;
+    Gtk::ComboBoxText* destinationSelect = nullptr;
+    Gtk::SpinButton* bsSelect = nullptr;
+    Gtk::Button* agreeButton = nullptr;
+    Gtk::Button* goButton = nullptr;
+
+    [[maybe_unused]] Window(BaseObjectType* super, const Glib::RefPtr<Gtk::Builder>& builder);
+
+    void agreeOverwriting() const;
+
+    void runDD() const;
+
+    void onSelectChanged(Gtk::ComboBoxText* widget);
+    void onSourceChanged() {
+      onSelectChanged(sourceSelect);
+    }
+
+    void onDestinationChanged() {
+      onSelectChanged(destinationSelect);
+    }
+  };
+
+  DDGUI(): Gtk::Application("net.overlisted.ddgui", Gio::APPLICATION_FLAGS_NONE) {}
+
+  Window* createWindow();
+
+  void on_activate() override;
+};
