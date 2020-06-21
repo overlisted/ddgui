5a104d74cd01997247db57b877a59cd85fad4df6
Ilya Maximov <mail@overlisted.net>
2020-06-21 12:46:13 +0200

    improve dd.h

--- dd.h
+++ dd.h
@@ -6,14 +6,14 @@
 std::vector<std::filesystem::path> getAllDevices();
 
 struct DDSession {
-  Glib::RefPtr<Gio::FileInputStream> source;
-  Glib::RefPtr<Gio::FileOutputStream> destination;
-
+  const Glib::RefPtr<Gio::FileInputStream> source;
+  const Glib::RefPtr<Gio::FileOutputStream> destination;
+  const gsize bufferSize;
   const gsize size;
+
   goffset progress = 0;
 
   char* buffer;
-  size_t bufferSize;
 
   DDSession(const Glib::RefPtr<Gio::File>& source, const Glib::RefPtr<Gio::File>& destination, gsize bufferSize);
   ~DDSession();
